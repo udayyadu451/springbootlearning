1fe419ecaf481ebabde32cd82d16f44e6c8f16df
uday.kondaveeti <uday.kondaveeti@aa.com>
2020-04-04 19:15:26 -0500

    removed unwanted imports

--- src/main/java/com/remington/springbootdemo/config/SwaggerConfig.java
+++ src/main/java/com/remington/springbootdemo/config/SwaggerConfig.java
@@ -8,8 +8,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
